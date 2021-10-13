ad11466333d7db45c64c19e7068b63aebfac2eb4
Luke Joseph <ldjdev12@gmail.com>
2021-10-13 13:54:35 +0100

    Reading from Pub-sub

---
+++ app/__init__.py
@@ -0,0 +1,34 @@
+import os
+from google.cloud import pubsub_v1
+from app.logger import logging_config
+
+logging_config()
+
+project_id = os.getenv('PROJECT_ID', 'ons-sdx-sandbox')
+subscription_id = "survey-subscription"
+
+
+class Config:
+    """Class to hold required configuration data"""
+
+    def __init__(self, proj_id) -> None:
+        self.PROJECT_ID = proj_id
+        self.SURVEY_SUBSCRIBER = None
+        self.SURVEY_SUBSCRIPTION_PATH = None
+
+
+CONFIG = Config(project_id)
+
+
+def cloud_config():
+    """
+    Loads configuration required for running against GCP based environments
+
+    This function makes calls to GCP native tools such as Google Secret Manager
+    and therefore should not be called in situations where these connections are
+    not possible, e.g running the unit tests locally.
+    """
+
+    survey_subscriber = pubsub_v1.SubscriberClient()
+    CONFIG.SURVEY_SUBSCRIPTION_PATH = survey_subscriber.subscription_path(project_id, subscription_id)
+    CONFIG.SURVEY_SUBSCRIBER = survey_subscriber
--- run.py
+++ run.py
@@ -1,3 +1,6 @@
+from app import subscriber, cloud_config
 
 if __name__ == '__main__':
     print('Starting sdx-eq-converter')
+    cloud_config()
+    subscriber.start()
