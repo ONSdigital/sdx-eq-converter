8b27c1c7aa9c993519a6fdf3a68e643a71c383f1
Luke Joseph <ldjdev12@gmail.com>
2021-10-18 16:02:58 +0100

    refactoring of unused lines

--- app/eq_converter.py
+++ app/eq_converter.py
@@ -1,5 +1,3 @@
-import hashlib
-
 import structlog
 
 from app.store import write_to_bucket
@@ -7,10 +5,7 @@ from app.store import write_to_bucket
 logger = structlog.get_logger()
 
 
-def process(tx_id : str, data_bytes: bytes):
+def process(tx_id: str, data_bytes: bytes):
 
     logger.info("Storing to bucket")
-    path = write_to_bucket(filename=tx_id)
-
-    logger.info("Sending DAP notification")
-
+    write_to_bucket(data=data_bytes, filename=tx_id)
--- app/store.py
+++ app/store.py
@@ -6,16 +6,12 @@ from app import CONFIG
 logger = structlog.get_logger()
 
 
-def write_to_bucket(data: str, filename: str) -> str:
+def write_to_bucket(data: bytes, filename: str):
     """
     Uploads a string submission to the correct folder within the GCP outputs bucket.
     """
     logger.info("Uploading to bucket")
-    # remove destination suffix
-    name = filename.split(":")[0]
-    path = f"{name}"
-    logger.info(f"Storing as {path}")
-    blob = CONFIG.BUCKET.blob(path)
+    logger.info(f"Storing as {filename}")
+    blob = CONFIG.BUCKET.blob(filename)
     blob.upload_from_string(data)
     logger.info(f"Successfully uploaded: {filename}")
-    return path
